1a5d0450c2e053a3c4ca5b54b10c09bcbbe6e2fd
YCoro <cristhianmorales18@gmail.com>
2014-09-28 17:33:28 -0600

    User Configuration

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -72,6 +72,45 @@ module.exports = {
         client.end();
       });
     });
-  }
+  },
+  configure : function(req, res){
+    var info = req.body.params;
+    var conString = "pg://postgres:cris12345@localhost:5432/GuateVuelos";
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+      if(err) {
+        return console.error('could not connect to postgres', err);
+      }
+      var query = client.query("SELECT * FROM USUARIOS WHERE usuario='"+info.username+"'", function(err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+        if(result.rows.length == 0){
+           res.json({state:0});
+        }
+        else{
+            var query = client.query("UPDATE USUARIOS SET nombre='"+info.name+"', apellido='"+info.lastName+"', correoelectronico='"+info.email+"', tarjetadecredito='"+info.tarjeta+"', numeropasaporte='"+info.documento+"', password='"+info.password+"' WHERE usuario='"+info.username+"';", function(err, result) {
+                var query = client.query("SELECT * FROM USUARIOS WHERE usuario='"+info.username+"'", function(err, result) {
+                if(err) {
+                   return console.error('error running query', err);
+                }
+
+                info.state = 200;
+                info.superuser = result.rows[0].superuser;
+                info.name = result.rows[0].nombre;
+                info.lastName = result.rows[0].apellido;
+                info.username = result.rows[0].usuario;
+                info.email = result.rows[0].correoelectronico;
+                info.tarjeta= result.rows[0].tarjetadecredito;
+                info.documento = result.rows[0].numeropasaporte;
+                info.nit = result.rows[0].nit;
+                    res.send(info);
+                });
+        });
+        }
+          client.end();
+      });
+    });
+  } 
 };
 
