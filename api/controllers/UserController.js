4ef26d0db376b33a7d29fde0c44359adc59c3c1b
Jimmy Morales <jimmymorales26@gmail.com>
2014-09-28 11:02:59 -0600

    register, login, parser

---
+++ api/controllers/UserController.js
@@ -0,0 +1,22 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	signUp : function (req, res) {
+    // body...
+    var info = req.body.params;
+
+    res.json({status:200});
+  },
+  signUp : function (req, res) {
+    // body...
+    var info = req.body.params;
+
+    res.json({status:200});
+  }
+};
+
