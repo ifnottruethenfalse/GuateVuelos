2dfd73777a9fd4802812f82dfd4227c31970e0fc
Jimmy Morales <jimmymorales26@gmail.com>
2014-09-28 00:51:24 -0600

    billetTab

--- assets/js/guateVuelosApp.js
+++ assets/js/guateVuelosApp.js
@@ -1,21 +1,50 @@
-var gvApp = angular.module("gvApp", ["ngRoute", "ngMaterial"]) // array is require
+var gvApp = angular.module("gvApp", ["ngMaterial","ui.date"]); // array is require
 
-gvApp.config(function($routeProvider){
-  
-  $routeProvider.when("/", {
-    templateUrl: "partials/index.html",
-    controller: "indexCtrl"
-  }).
-  otherwise({
-    redirectTo: "/"
-  });
-
-});
-
-gvApp.controller("gvAppCtrl",['$scope', function ($scope){
-
+gvApp.controller("gvAppCtrl",['$scope','$materialDialog', function ($scope,$materialDialog){
+  $scope.selectedIndex = 0;
+  $scope.signUp = function (e) {
+    $materialDialog({
+      templateUrl: 'partials/sign-up.html',
+      targetEvent: e,
+      controller: ['$scope', '$hideDialog', function($scope, $hideDialog) {
+        $scope.close = function() {
+          $hideDialog();
+        };
+      }]
+    });
+  };
 }]);
 
-gvApp.controller("indexCtrl",['$scope', function ($scope){
+gvApp.controller("billetCtrl",['$scope','$http', function ($scope,$http){
+  $scope.onSearch = false;
+  $scope.searchFlight = function() {  
+    $scope.onSearch = true;
+  };
+  
+  /*$scope.disabled = undefined;
 
+  $scope.enable = function() {
+    $scope.disabled = false;
+  };
+
+  $scope.disable = function() {
+    $scope.disabled = true;
+  };
+
+  $scope.clear = function() {
+    $scope.person.selected = undefined;
+    $scope.address.selected = undefined;
+    $scope.country.selected = undefined;
+  };
+
+  $scope.address = {};
+  $scope.refreshAddresses = function(address) {
+    var params = {address: address, sensor: false};
+    return $http.get(
+      'http://maps.googleapis.com/maps/api/geocode/json',
+      {params: params}
+    ).then(function(response) {
+      $scope.addresses = response.data.results
+    });
+  };*/
 }]);
